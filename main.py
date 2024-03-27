5630a07ab4264daec521707d2b69d28cc67c9fa7
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-03-27 13:40:50 +1000

    updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from RoiMultiClass import ComposeROI
 from video_stream2 import VideoProcessor
@@ -9,11 +10,27 @@ def main():
         data = json.load(f)
 
     roi_config = ComposeROI(data)
-
     video_path = roi_config.video_file
-    output_path = data.get("output_video_path", "crusher_bin_bridge_2.mkv")
 
-    video_processor = VideoProcessor(video_path, output_path, roi_config)
+    if not os.path.exists(video_path) or not os.path.isfile(video_path):
+        print("Input video path or file does not exist.")
+        return
+
+    output_path = data.get("output_video_path", None)
+
+    if output_path is None:
+        output_dir = data.get("output_video_dir", "output")
+        os.makedirs(output_dir, exist_ok=True)
+        output_path = os.path.join(output_dir, "output_video.mkv")
+
+    is_watching = data.get("is_watching", False)
+    is_save_video = data.get("is_save_video", False)
+    offset = data.get("offset", 0)
+
+    video_processor = VideoProcessor(video_path, output_path, roi_config,
+                                     is_watching=is_watching,
+                                     is_save_video=is_save_video,
+                                     offset=offset)
     video_processor.process_video()
 
 
--- video_stream2.py
+++ video_stream2.py
@@ -1,29 +1,28 @@
 import cv2
-
 from process_roi import FrameProcessor
 
 
 class VideoProcessor:
-    def __init__(self, video_path, output_path, roi_comp):
+    def __init__(self, video_path, output_dir, roi_comp, is_watching=False, is_save_video=False, offset=0):
         self.video_path = video_path
-        self.output_path = output_path
+        self.output_dir = output_dir
         self.roi_comp = roi_comp
+        self.is_watching = is_watching
+        self.is_save_video = is_save_video
+        self.offset = offset
         self.frame_processor = FrameProcessor(roi_comp)
 
-    def capture_video(self):
-        cap = cv2.VideoCapture(self.video_path)
-        return cap
-
     def process_video(self):
-        cap = self.capture_video()
+        out = None
+        cap = cv2.VideoCapture(self.video_path)
+        cap.set(cv2.CAP_PROP_POS_MSEC, self.offset * 1.0e3)
 
-        cap.set(cv2.CAP_PROP_POS_MSEC, 700 * 1.0e3)
-
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        out = cv2.VideoWriter(self.output_path, fourcc, fps, (frame_width, frame_height))
+        if self.is_save_video:
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            out = cv2.VideoWriter(self.output_dir, fourcc, fps, (frame_width, frame_height))
 
         while cap.isOpened():
             ret, frame = cap.read()
@@ -31,18 +30,22 @@ class VideoProcessor:
                 break
 
             processed_frame = self.frame_processor.process_frame(frame)
-            cv2.imshow('Filtered Frame ', processed_frame)
-            out.write(processed_frame)
+
+            if self.is_watching:
+                cv2.imshow('Filtered Frame ', processed_frame)
+
+            if self.is_save_video:
+                out.write(processed_frame)
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
 
         cap.release()
-        out.release()
+        if self.is_save_video:
+            out.release()
         cv2.destroyAllWindows()
 
-
-
-
+    def start(self):
+        self.process_video()
 
 
